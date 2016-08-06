c9c3b9e5b003592b56b159cae0f01069abad14d6
girishdubey <dubey.girish2@gmail.com>
2016-08-06 20:22:32 +0530

    make a sum of two no

--- ConsoleFirstAppliation/Program.cs
+++ ConsoleFirstAppliation/Program.cs
@@ -10,6 +10,13 @@ namespace ConsoleFirstAppliation
     {
         static void Main(string[] args)
         {
+            int no1, no2;
+            int total = 0;
+            no1 = 5;
+            no2 = 10;
+            total = no1 + no2;
+            Console.WriteLine(total);
+            Console.ReadLine();
         }
     }
 }
