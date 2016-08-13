7ec85d84d29d060e2006f1197aa20e852fe36f1e
girishdubey <dubey.girish2@gmail.com>
2016-08-13 21:58:11 +0530

    Second Class
    For loop

--- ConsoleFirstAppliation/LoopClass.cs
+++ ConsoleFirstAppliation/LoopClass.cs
@@ -10,19 +10,24 @@ namespace ConsoleFirstAppliation
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("************While Loop******************");
             WhileLoopExample1();
+            Console.WriteLine("************While Loop******************");
             WhileLoopExample2();
+            Console.WriteLine("************Do While Loop******************");
             DoWhileLoopExample1();
+            Console.WriteLine("************For Loop******************");
+            ForWhileLoopExample1();
         }
 
         public static void WhileLoopExample1()
         {
 
             int i = 0;
-            while (i < 10) 
-            { 
-                Console.WriteLine(i); 
-                i++; 
+            while (i < 10)
+            {
+                Console.WriteLine(i);
+                i++;
             }
 
             Console.ReadLine();
@@ -37,7 +42,7 @@ namespace ConsoleFirstAppliation
             int CurrPendingAmount = TotalPendingAmount;
 
 
-            while (PaidAmout < TotalPendingAmount) 
+            while (PaidAmout < TotalPendingAmount)
             {
                 Console.Write("Enter pay amount (" + CurrPendingAmount + "): ");
                 PayAmout = Convert.ToInt32(Console.ReadLine());
@@ -48,7 +53,7 @@ namespace ConsoleFirstAppliation
             Console.WriteLine("******************************");
             Console.WriteLine("Pending Amout: " + TotalPendingAmount);
             Console.WriteLine("Paid Amout: " + PaidAmout);
-            Console.WriteLine("Extra Amout: " + (PaidAmout-TotalPendingAmount));
+            Console.WriteLine("Extra Amout: " + (PaidAmout - TotalPendingAmount));
             Console.WriteLine("******************************");
 
             Console.ReadLine();
@@ -71,5 +76,44 @@ namespace ConsoleFirstAppliation
         }
 
 
+        public static void ForWhileLoopExample1()
+        {
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+
+        }
+
+
+        public static void ForWhileLoopExample2()
+        {
+
+            int i = 0;
+
+            for (; ; )
+            {
+                Console.WriteLine(i);
+                if (i < 10)
+                    break;
+                i++;
+            }
+
+            Console.ReadLine();
+
+        }
+
+
+        public static void ForWhileLoopExample3()
+        {
+            for (int i = 0; i < 10; i++)
+                Console.WriteLine(i);
+
+            Console.ReadLine();
+        }
+
     }
-} 
+}
